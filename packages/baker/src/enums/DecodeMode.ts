e82ded026d2ff7e0370da4b178b1e892ed63397c
zhuxudong <callzhuxudong@163.com>
2023-03-24 21:43:23 +0800

    feat: add sh

---
+++ packages/baker/src/enums/DecodeMode.ts
@@ -0,0 +1,6 @@
+export enum DecodeMode {
+  Linear,
+  Gamma,
+  RGBE,
+  RGBM
+}
--- packages/baker/src/index.ts
+++ packages/baker/src/index.ts
@@ -1,6 +1,7 @@
 import "./loader/HDRRGBELoader";
 export { BakerResolution } from "./enums/BakerResolution";
+export { DecodeMode } from "./enums/DecodeMode";
 export { IBLBaker } from "./IBLBaker";
-// export { SphericalHarmonics3Baker } from "./SphericalHarmonics3Baker";
+export { SphericalHarmonics3Baker } from "./SphericalHarmonics3Baker";
 export { downloadArrayBuffer } from "./utils/downloadArrayBuffer";
 export { toBuffer } from "./utils/toBuffer";
