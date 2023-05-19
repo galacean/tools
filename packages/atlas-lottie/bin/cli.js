371ef20e160db5da9c39b3531fde13e43d94e792
leoooy <leoy.dev@gmail.com>
2023-05-19 16:48:18 +0800

    feat: atlas-lottie add size limit args

--- packages/atlas-lottie/bin/cli.js
+++ packages/atlas-lottie/bin/cli.js
@@ -13,9 +13,11 @@ program.version(packageJson.version);
 program
   .option("-s, --src <src>", "the lottie JSON file")
   .option("-i, --images <images>", "the image files")
+  .option("-mw, --maxWidth <maxWidth>", "the max width limit, default 1024")
+  .option("-mh, --maxHeight <maxHeight>", "the max height limit, default 1024")
   .description("Transform lottie file to galacean atlas format")
   .action((argv) => {
-    transform(argv.src, argv.images);
+    transform(argv.src, argv.images, { maxWidth: argv.maxWidth || 1024, maxHeight: argv.maxHeight || 1024 });
   });
 
 program.parse(process.argv);
